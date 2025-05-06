2f735cb9132eb3ef02e6e7e4101b339b16907b74
hannahli11 <hannahli16492@gmail.com>
2025-05-06 09:56:34 -0700

    port

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,10 +1,10 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-        pythonURI = "http://localhost:8887";
+        pythonURI = "http://localhost:8101";
 } else if (location.hostname === "127.0.0.1") {
-        pythonURI = "http://127.0.0.1:8887";
+        pythonURI = "http://127.0.0.1:8101";
 } else {
-        pythonURI =  "https://flocker.nighthawkcodingsociety.com";
+        pythonURI =  "https://dnadynamite.stu.nighthawkcodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
